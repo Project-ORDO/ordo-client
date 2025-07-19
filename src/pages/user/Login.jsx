0c077e17a392276cb72231b5a04d05a2ce42f96e
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-19 19:50:16 +0530

    fix: changed the login to use vaiable color and button

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -46,13 +46,13 @@ const LoginPage = () => {
     />
      {/* Social Login Buttons BELOW the form */}
       <div className="mt-6 space-y-3 text-center">
-        <p className="text-gray-500 text-sm">Or</p>
+        <p className="text-[rgb(var(--text-primary))]  text-sm">Or</p>
 
-        <button className="w-full border py-2 rounded hover:bg-gray-100 font-medium">
+        <button className="w-full border py-2 rounded btn-outline font-medium">
           Continue with Google
         </button>
 
-        <button className="w-full border py-2 rounded hover:bg-gray-100 font-medium">
+        <button className="w-full border py-2 rounded btn-outline font-medium">
           Continue with GitHub
         </button>
       </div>
