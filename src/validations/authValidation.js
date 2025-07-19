9704c3dd7c6f43a0c5c9c14ce1ea89bbfd42a959
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-19 16:42:08 +0530

    fix: made the username not to give any space or special charecters

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -2,7 +2,13 @@ import * as yup from "yup";
 
 // Signup schema
 export const signupSchema = yup.object().shape({
-  username: yup.string().required("Username is required"),
+  username: yup
+    .string()
+    .required("Username is required")
+    .matches(
+      /^[a-zA-Z0-9_]+$/,
+      "Username can only contain letters, numbers, and underscores (no spaces or special characters)"
+    ),
   email: yup.string().email("Invalid email").required("Email is required"),
   password: yup
     .string()
