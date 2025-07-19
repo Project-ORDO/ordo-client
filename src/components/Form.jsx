9379dd9a70d007e597c3d5077573a72fa0b45415
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-19 19:40:17 +0530

    fix: changed the form background to scale with dark mode

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,19 +16,19 @@ const AuthForm = ({
   } = useForm();
 
   return (
-    <div className="max-w-md mx-auto p-6  rounded-2xl shadow-soft container-app">
+    <div className="max-w-md mx-auto p-6 --background   rounded-2xl shadow-soft container-app" style={{ backgroundColor: "rgb(var(--surface))" }}>
       <h2 className="text-2xl font-bold mb-6 text-primary-content text-center">{heading}</h2>
 
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
         {fields.map((field) => (
           <div key={field.name}>
-            <label className="block text-sm text-primary-content font-medium mb-1">
+            <label className="block text-sm text-primary-content font-medium mb-1 ">
               {field.label}
             </label>
             <input
               type={field.type || "text"}
               {...register(field.name, field.validation)}
-              className="w-full p-2 text-primary-content border input  rounded"
+              className="w-full p-2  border  input  rounded dark:bg-[rgb(var(--surface))] dark:text-white"
               placeholder={field.placeholder}
             />
             {errors[field.name] && (
@@ -41,7 +41,7 @@ const AuthForm = ({
 
         <button
           type="submit"
-          className="w-full py-2 px-4 btn-primary rounded"
+          className="w-full py-2 px-4 btn-primary rounded  .btn-primary:hover"
         >
           {submitText}
         </button>
