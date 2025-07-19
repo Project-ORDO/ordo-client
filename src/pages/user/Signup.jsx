68b624b0708bf42b52ac3044414dfe7baa84b4db
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-19 19:50:43 +0530

    fix: changed the sigup to use vaiable color and button

--- src/pages/user/Signup.jsx
+++ src/pages/user/Signup.jsx
@@ -49,14 +49,14 @@ const SignupPage = () => {
         ]}
       />
       {/* Social Login Buttons BELOW the form */}
-      <div className="mt-6 space-y-3 text-center">
-        <p className="text-gray-500 text-sm">Or</p>
+     <div className="mt-6 space-y-3 text-center">
+        <p className="text-[rgb(var(--text-primary))]  text-sm">Or</p>
 
-        <button className="w-full border py-2 rounded hover:bg-gray-100 font-medium">
+        <button className="w-full border py-2 rounded btn-outline font-medium">
           Continue with Google
         </button>
 
-        <button className="w-full border py-2 rounded hover:bg-gray-100 font-medium">
+        <button className="w-full border py-2 rounded btn-outline font-medium">
           Continue with GitHub
         </button>
       </div>
